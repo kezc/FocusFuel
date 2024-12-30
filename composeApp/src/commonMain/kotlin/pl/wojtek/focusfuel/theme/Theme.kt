7da81ad4561d09fde42cd61d1f9f5074444774d9
Wojciech Krystyniak <wojciechkrystyniak@gmail.com>
2024-12-30 19:23:18 +0100

    Change app colors

--- composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/theme/Theme.kt
@@ -12,21 +12,42 @@ fun AppTheme(
 ) {
     val colorScheme = if (darkTheme) {
         darkColorScheme(
-            primary = Color(0xFF86CEFF),
-            primaryContainer = Color(0xFF004B70),
-            secondary = Color(0xFF97CBFF),
-            secondaryContainer = Color(0xFF004B70),
-            tertiary = Color(0xFFBBC7DB),
-            tertiaryContainer = Color(0xFF254B6E)
+            primary = Color(0xFFD32F2F),
+            primaryContainer = Color(0xFFB71C1C),
+            secondary = Color(0xFFEF5350),
+            secondaryContainer = Color(0xFFEF9A9A),
+            tertiary = Color(0xFFFFCDD2),
+            tertiaryContainer = Color(0xFFFFEBEE)
         )
     } else {
         lightColorScheme(
-            primary = Color(0xFF006590),
-            primaryContainer = Color(0xFFC9E6FF),
-            secondary = Color(0xFF006590),
-            secondaryContainer = Color(0xFFD1E4FF),
-            tertiary = Color(0xFF3B5C7E),
-            tertiaryContainer = Color(0xFFD3E4FF)
+            primary = Color(0xFFD32F2F),
+            onPrimary = Color.White,
+            primaryContainer = Color(0xFFEF5350),
+            onPrimaryContainer = Color.White,
+            inversePrimary = Color(0xFFFFC107),
+            secondary = Color(0xFFB71C1C),
+            onSecondary = Color.White,
+            secondaryContainer = Color(0xFFEF9A9A),
+            onSecondaryContainer = Color.Black,
+            tertiary = Color(0xFFFFCDD2),
+            onTertiary = Color.Black,
+            tertiaryContainer = Color(0xFFFFEBEE),
+            onTertiaryContainer = Color.Black,
+            background = Color(0xFFFFE0E0),
+            onBackground = Color.Black,
+            surface = Color(0xFFFFC0C0),
+            onSurface = Color.Black,
+            surfaceVariant = Color(0xFFFFC1C1),
+            onSurfaceVariant = Color.Black,
+            error = Color(0xFFD32F2F),
+            onError = Color.White,
+            outline = Color(0xFFB71C1C),
+            surfaceContainer = Color(0xFFFFB2B2),
+            surfaceContainerHigh = Color(0xFFFF9999),
+            surfaceContainerHighest = Color(0xFFFF7F7F),
+            surfaceContainerLow = Color(0xFFFFC1C1),
+            surfaceContainerLowest = Color(0xFFFFE0E0),
         )
     }
 
