421794d3964807905db46d61b1f70b7ec5cfdd50
Wojciech Krystyniak <wojciechkrystyniak@gmail.com>
2025-01-01 18:43:52 +0100

    Add index to productId

--- composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/database/model/PurchaseEntity.kt
+++ composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/database/model/PurchaseEntity.kt
@@ -2,10 +2,14 @@ package pl.wojtek.focusfuel.database.model
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import kotlinx.datetime.LocalDateTime
 
-@Entity(foreignKeys = [ForeignKey(entity = ProductEntity::class, parentColumns = ["id"], childColumns = ["productId"])])
+@Entity(
+    foreignKeys = [ForeignKey(entity = ProductEntity::class, parentColumns = ["id"], childColumns = ["productId"])],
+    indices = [Index("productId")]
+)
 data class PurchaseEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val productId: Long,
