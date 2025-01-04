c7f25715b3c6d66366aa4854a87bf7c7b443febe
Wojciech Krystyniak <wojciechkrystyniak@gmail.com>
2025-01-04 19:33:21 +0100

    Add more predefined treats

--- composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/pl/wojtek/focusfuel/database/AppDatabase.kt
@@ -37,7 +37,17 @@ class AppDatabaseCallback : RoomDatabase.Callback() {
     override fun onCreate(connection: SQLiteConnection) {
         super.onCreate(connection)
         val statement = connection
-            .prepare("INSERT INTO ProductEntity (id, name, costInPomodoros, hidden) VALUES (1, 'Product 1', 5, 0)")
+            .prepare("" +
+                    "INSERT INTO ProductEntity (id, name, costInPomodoros, hidden) VALUES" +
+                    "(1, 'Reward yourself with 1 hour of gaming', 3, 0)," +
+                    "(2, 'Treat yourself to a delicious bar of chocolate', 2, 0)," +
+                    "(3, 'Take a day off from studying or chores', 8, 0)," +
+                    "(4, 'Watch 1 episode of a TV show', 4, 0)," +
+                    "(5, 'Spend 30 minutes on a creative activity', 4, 0)," +
+                    "(6, 'Get a new book', 5, 0)," +
+                    "(7, 'Order your favorite snack or meal', 6, 0)," +
+                    "(8, 'Enjoy a relaxing walk in the park', 6, 0)," +
+                    "(9, 'Treat yourself to a movie or special event', 10, 0);")
         while (statement.step());
     }
 }
