750cd070227f4065a60ffab134395a59716ce0ec
Wojciech Krystyniak <wojciechkrystyniak@gmail.com>
2025-01-05 18:03:36 +0100

    Remove log

--- composeApp/src/androidMain/kotlin/pl/wojtek/focusfuel/notification/BackgroundNotificationManager.kt
+++ composeApp/src/androidMain/kotlin/pl/wojtek/focusfuel/notification/BackgroundNotificationManager.kt
@@ -1,6 +1,5 @@
 package pl.wojtek.focusfuel.notification
 
-import android.util.Log
 import me.tatarka.inject.annotations.Inject
 import pl.wojtek.focusfuel.notifications.NotificationSender
 import pl.wojtek.focusfuel.notifications.SimpleNotificationSender
@@ -12,7 +11,6 @@ class BackgroundNotificationSender(
     private val appInForegroundNotifier: AppInForegroundNotifier,
 ): NotificationSender {
     override fun sendNotification(title: String, body: String, id: Int) {
-        Log.d("DUPA", "appInForegroundNotifier.isAppInForeground: ${appInForegroundNotifier.isAppInForeground}")
         if (!appInForegroundNotifier.isAppInForeground) {
             simpleNotificationSender.sendNotification(title, body, id)
         } else {
